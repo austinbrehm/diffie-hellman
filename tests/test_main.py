a3a58e12bd710ba9e695fde92681128b0b3c0f36
Austin Brehm <austin.brehm@gmail.com>
2022-10-30 21:01:48 -0600

    add edge cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,8 @@ import main
 
 class TestMain(TestCase):
     def test_diffie_hellman(self):
-        result = main.diffie_hellman(5, 10, 15, 20)
-        self.assertEqual(result, 5)
+        self.assertEqual(main.diffie_hellman(1, 2, 3, 4), 1)
+        self.assertEqual(main.diffie_hellman(5, 10, 15, 20), 5)
+        self.assertEqual(main.diffie_hellman(145, 264, 354, 564), 252)
+
 
